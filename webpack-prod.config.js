d6c5a67256432af90352c44489bd2188bcea6019
Vini Falavigna <vini.df@gmail.com>
2020-02-09 14:40:29 +1100

    initial commit

---
+++ webpack-prod.config.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  mode: "production",
+
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./templates/index.ejs",
+      title: "Development",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+}
